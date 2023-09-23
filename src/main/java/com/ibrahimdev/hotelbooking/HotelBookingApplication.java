b99efc71a62712e297858fbf50ac7dfd65247746
ibrahimdevl <ibrahimhoussem2000@gmail.com>
2023-09-23 21:52:08 +0100

    initial commit

---
+++ src/main/java/com/ibrahimdev/hotelbooking/HotelBookingApplication.java
@@ -0,0 +1,13 @@
+package com.ibrahimdev.hotelbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelBookingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelBookingApplication.class, args);
+    }
+
+}
