21c3266d70e5a190fb588054c603812a40a1feec
ibrahimdevl <ibrahimhoussem2000@gmail.com>
2023-10-09 23:07:36 +0100

    test user registration

---
+++ src/test/java/com/ibrahimdev/hotelbooking/controller/AuthControllerRegisterUserTest.java
@@ -0,0 +1,48 @@
+package com.ibrahimdev.hotelbooking.controller;
+
+import static org.mockito.Mockito.when;
+
+import com.ibrahimdev.hotelbooking.dto.PersonDTO;
+import com.ibrahimdev.hotelbooking.dto.SignUp;
+import com.ibrahimdev.hotelbooking.service.UserService;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class AuthControllerRegisterUserTest {
+
+    @Mock
+    private UserService userServiceMock;
+
+    @Test
+    public void shouldRegisterUserSuccessfully() {
+        SignUp signUpDto = new SignUp();
+        signUpDto.setFirstName("Houssem");
+        signUpDto.setLastName("Ibrahim");
+        signUpDto.setUsername("ibrahimdev");
+        signUpDto.setEmail("ibrahimdev@example.com");
+        signUpDto.setPassword("password");
+
+
+        PersonDTO personDTO = new PersonDTO();
+        personDTO.setId(1L);
+        personDTO.setUsername("ibrahimdev");
+        personDTO.setEmail("ibrahimdev@example.com");
+        personDTO.setFirstName("Houssem");
+        personDTO.setLastName("Ibrahim");
+
+        when(userServiceMock.registerUser(signUpDto)).thenReturn(personDTO);
+
+        AuthController authController = new AuthController(userServiceMock);
+        ResponseEntity<PersonDTO> responseEntity = authController.registerUser(signUpDto);
+
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        assertThat(responseEntity.getBody()).isEqualTo(personDTO);
+    }
+}
